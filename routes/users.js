75d21bcff8eeceae8942e2c4a3f7375bafd985af
EgorStogov <egst94@yandex.ru>
2022-07-26 22:08:04 +0300

    feature: add controller's methods

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,15 @@
 const { celebrate, Joi } = require('celebrate');
 const router = require('express').Router();
 
-router.get('/users/me', );
+const { getMe, updateUserProfile } = require('../controllers/users');
+
+router.get('/users/me', getMe);
 
 router.patch('/users/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     email: Joi.string().required().email()
   })
-}));
+}), updateUserProfile);
 
 module.exports = router;
