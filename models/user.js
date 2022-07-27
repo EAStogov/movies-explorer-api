ce37497c8d00e2e6a0550e23c29f45c443713bfb
EgorStogov <egst94@yandex.ru>
2022-07-28 00:32:23 +0300

    feature: add search by credentials

--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UnauthorizedError = require('../errors/UnauthorizedError');
 
 const userSchema = mongoose.Schema({
   email: {
@@ -18,5 +21,23 @@ const userSchema = mongoose.Schema({
     maxlength: 30
   }
 })
+
+function findUserByCredentials(email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
+          }
+          return user;
+        });
+    });
+}
+
+userSchema.statics.findUserByCredentials = findUserByCredentials;
 
 module.exports = mongoose.model('user', userSchema);
