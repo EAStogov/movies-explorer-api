002bdd106f09de624881b82625edb89d414b692b
EgorStogov <egst94@yandex.ru>
2022-07-30 13:34:57 +0300

    reafactor: remove extra next()

--- controllers/users.js
+++ controllers/users.js
@@ -61,8 +61,6 @@ const createUser = (req, res, next) => {
         .catch((err) => {
           if (err.name === 'ValidationError') {
             next(new BadRequestError('Введены некорректные данные'));
-          } else {
-            next(err);
           }
         });
       return res.send({
