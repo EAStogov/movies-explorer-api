5eee42d28c8b8a9b88c5916e6615e4a6d63fa93b
EgorStogov <egst94@yandex.ru>
2022-07-31 16:33:40 +0300

    feature: use validator for links validation

--- routes/movies.js
+++ routes/movies.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 
 const { celebrate, Joi } = require('celebrate');
+const validator = require('validator');
+
 const { getMovies, postMovie, deleteMovie } = require('../controllers/movies');
 
 router.get('', getMovies);
@@ -12,9 +14,24 @@ router.post('', celebrate({
     duration: Joi.number().required(),
     year: Joi.string().required(),
     description: Joi.string().required(),
-    image: Joi.string().required().uri(),
-    trailerLink: Joi.string().required().uri(),
-    thumbnail: Joi.string().required().uri(),
+    image: Joi.string().required().custom((value, helpers) => {
+      if (validator.isURL(value)) {
+        return value;
+      }
+      return helpers.message('Некорректная ссылка на изображение');
+    }),
+    trailerLink: Joi.string().required().custom((value, helpers) => {
+      if (validator.isURL(value)) {
+        return value;
+      }
+      return helpers.message('Некорректная ссылка на трейлер');
+    }),
+    thumbnail: Joi.string().required().custom((value, helpers) => {
+      if (validator.isURL(value)) {
+        return value;
+      }
+      return helpers.message('Некорректная ссылка на постер');
+    }),
     movieId: Joi.number().required(),
     nameRU: Joi.string().required(),
     nameEN: Joi.string().required(),
