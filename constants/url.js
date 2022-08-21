a4e2811c7b2b1f0326c7d27747b6631d612c22d4
EgorStogov <egst94@yandex.ru>
2022-08-21 21:01:07 +0300

    feature: add urls for cors

--- constants/url.js
+++ constants/url.js
@@ -3,7 +3,7 @@ const urls = [
   'https://esto.movie.nomoredomains.xyz',
   'http://api.esto.movie.nomoredomains.xyz',
   'https://api.esto.movie.nomoredomains.xyz',
-  'localhost:3000',
+  'http://localhost:3000',
   'localhost:3001',
 ];
 
