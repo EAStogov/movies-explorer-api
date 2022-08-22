189e735dac60cbebf31eb6ed2ac9e106efb4072d
EgorStogov <egst94@yandex.ru>
2022-08-22 12:39:36 +0300

    reafctor: test auth #4

--- midlewares/auth.js
+++ midlewares/auth.js
@@ -19,7 +19,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, secret);
   } catch (err) {
-    throw new UnauthorizedError(`cookies: ${cookies}`);
+    throw new UnauthorizedError(`cookies: ${token}`);
   }
 
   req.user = payload;
